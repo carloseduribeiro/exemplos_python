f652a6474aae280cf54d61bf60161216c8e27759
Carlos Eduardo <carloseribeiro96@gmail.com>
2021-04-15 17:05:47 -0300

    Métodos medicamento_entregue e definir_como_entregue adicionados;

--- AppFarmacia/Classes/Receita.py
+++ AppFarmacia/Classes/Receita.py
@@ -1,3 +1,5 @@
+from Classes.Farmaceutico import Farmaceutico
+
 class Receita:
 
     # informacoes_receita = {"medicamento": str, "validade": date, "quantidade": int, "paciente": Paciente}
@@ -6,3 +8,12 @@ class Receita:
         self.validade = informacoes["validade"]
         self.quantidade = informacoes["quantidade"]
         self.paciente = informacoes["paciente"]
+        self.__medicamento_entregue = False
+        self.farmaceutico = Farmaceutico
+
+    def medicamento_entregue(self) -> bool:
+        return self.__medicamento_entregue
+
+    def definir_como_entregue(self, farmaceutico: Farmaceutico):
+        self.__medicamento_entregue = True
+        self.farmaceutico = farmaceutico
